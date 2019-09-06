6f21aa0aa59e5c1f3d5f0ed61c2e97a2bb724fef
michaelarriaga <m.arriaga.smb@gmail.com>
2019-09-06 02:04:54 -0700

    193 styling odds and ends -finish

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { View, Text, Input, Button } from 'react-native-elements';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
   return (
-    <>
+    <View style={styles.container}>
       <Spacer>
         <Text h3>Sign Up For Tracker</Text>
       </Spacer>
@@ -18,11 +18,23 @@ const SignupScreen = ({ navigation }) => {
       <Spacer>
         <Button title='Sign Up'/>
       </Spacer>
-    </>
+    </View>
   )
 }
 
+SignupScreen.navigationOptions = () => {
+  return {
+    header: null
+  }
+}
+
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 200,
+    paddingHorizontal: 20
+  }
 
 })
 
