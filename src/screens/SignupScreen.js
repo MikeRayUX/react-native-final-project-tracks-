17a7ae9a075ba7586a89614cca1e6ab4e46277a6
michaelarriaga <m.arriaga.smb@gmail.com>
2019-09-06 01:49:33 -0700

    192 Helper Styling Components

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { View, Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
-  return <>
-    <Text style={{fontSize: 48}}>SignupScreen</Text>
-    <Button title="Go to signin" onPress={() => navigation.navigate('Signin')}/>
-    <Button title="Go to main flow" onPress={() => navigation.navigate('mainFlow')}/>
-  </>
+  return (
+    <>
+      <Spacer>
+        <Text h3>Sign Up For Tracker</Text>
+      </Spacer>
+
+      <Input label='Email'/>
+      <Spacer />
+
+      <Input label='Password'/>
+      
+      <Spacer>
+        <Button title='Sign Up'/>
+      </Spacer>
+    </>
+  )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+})
 
 export default SignupScreen;
