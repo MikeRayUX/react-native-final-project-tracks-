542204bd0281d7a8a65edfba71567db704b7a2cd
michaelarriaga <m.arriaga.smb@gmail.com>
2019-09-06 00:17:23 -0700

    188 A LOT of boilerplate -finish

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+
+const SignupScreen = () => {
+  return <Text styles={{fontSize: 48}}>SignupScreen</Text>
+}
+
+const styles = StyleSheet.create({})
+
+export default SignupScreen;
