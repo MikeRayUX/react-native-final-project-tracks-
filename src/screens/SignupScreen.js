7b3865ed2c13f8475e846c2063414f1cd89efab9
michaelarriaga <m.arriaga.smb@gmail.com>
2019-09-07 02:31:22 -0700

    207 creating a navlink -finish

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,9 +1,8 @@
 import React, { useContext } from 'react';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
-import { Text } from 'react-native-elements';
-import Spacer from '../components/Spacer';
 import { Context as AuthContext } from '../context/AuthContext';
 import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
 const SignupScreen = ({ navigation }) => {
   const { state, signup } = useContext(AuthContext);
@@ -12,19 +11,16 @@ const SignupScreen = ({ navigation }) => {
     <View style={styles.container}>
 
       <AuthForm
-        headerText="Sign Up For Tracker"
+        headerText='Sign Up For Tracker'
         errorMessage={state.errorMessage}
-        submitButtonText="Sign Up"
+        submitButtonText='Sign Up'
         onSubmit={signup}
       />
 
-      <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
-        <Spacer>
-          <Text style={styles.link}>
-            Already have an account? Sign in instead
-            </Text>
-        </Spacer>
-      </TouchableOpacity>
+      <NavLink 
+        text='Already have an account? Sign in instead!'
+        routeName='Signin'
+      />
     </View>
   )
 }
@@ -42,17 +38,6 @@ const styles = StyleSheet.create({
     marginBottom: 200,
     paddingHorizontal: 20
   },
-  errors: {
-    color: 'red',
-    fontSize: 18,
-    marginLeft: 15,
-    marginTop: 15
-  },
-  link: {
-    color: 'blue',
-    fontSize: 16
-  }
-
 })
 
 export default SignupScreen;
