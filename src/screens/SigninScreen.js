1a9f5fe4fd4ee499123ae1272470fad9dd813da1
michaelarriaga <m.arriaga.smb@gmail.com>
2019-09-07 02:41:25 -0700

    209 wiring up signin -finish

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Context as AuthContext } from '../context/AuthContext';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
 const SigninScreen = () => {
-  return <Text style={{ fontSize: 48 }}>SigninScreen</Text>
+  const { state, signin } = useContext(AuthContext);
+  return <View style={styles.container}>
+    <AuthForm 
+      headerText="Sign In To Tracker"
+      errorMessage={state.errorMessage}
+      submitButtonText='Sign In'
+      onSubmit={() => ""}
+    />
+
+    <NavLink
+      text="Don't have an account? Sign up Instead!"
+      routeName='Signup'
+    />
+  </View>
 }
 
-const styles = StyleSheet.create({})
+SigninScreen.navigationOptions = () => {
+  return {
+    header: null
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 250,
+    paddingHorizontal: 20
+  }
+})
 
 export default SigninScreen;
